8b4397531c56d8825ad18c0151677366d6fb47c9
JK-le-dev <jkcondor13@gmail.com>
2024-10-27 12:34:12 -0500

    Added VisionOS design to Library Website
    Formatted books in their own DOM elements. Added READ button and remove
    book button to Book element.
    
    Another feature is the change background button along with reset
    background to the original websites background.
    
    As for CSS and styling, it takes inspriation from VisionOS with many
    glassy elements and curved corners. I doubt I'll ever put this much
    effort into another odin project ;-;

--- javascript-course/library/script.js
+++ javascript-course/library/script.js
@@ -1,70 +1,177 @@
 let Library = [];
 
-// DOM Elements
-const Title = document.getElementById("title");
-const Author = document.getElementById("author");
-const Pages = document.getElementById("pages");
-const Read = document.getElementById("read");
-const List = document.getElementById("list");
-const Popup = document.getElementById("popup");
-
 class Book {
-  constructor(title, author, pages, read) {
+  constructor(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.readToString = read ? "Wait List" : "Finished";
+    this.id = id;
+  }
+
+  // DOM Element Creation Methods
+  appendTitle(parentDiv) {
+    var titleElement = document.createElement("p");
+    titleElement.innerText = '"' + this.title + '"';
+    parentDiv.appendChild(titleElement);
+  }
+  appendAuthor(parentDiv) {
+    var authorElement = document.createElement("p");
+    authorElement.innerText = "by " + this.author;
+    parentDiv.appendChild(authorElement);
+  }
+  appendPages(parentDiv) {
+    var pagesElement = document.createElement("p");
+    pagesElement.innerText = this.pages + " pages";
+    parentDiv.appendChild(pagesElement);
+  }
+  appendReadButton(parentDiv) {
+    var readButton = document.createElement("button");
+    var readToString = (read) => {
+      return read ? "Not Read" : "Read";
+    };
+    readButton.innerText = readToString(this.read);
+    readButton.addEventListener("click", () => {
+      this.read = !this.read;
+      readButton.innerText = readToString(this.read);
+    });
+    parentDiv.appendChild(readButton);
+  }
+  appendRemoveButton(parentDiv) {
+    var removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.addEventListener("click", () => {
+      const index = searchLibrary(this.createBookObject());
+      Library.splice(index, 1);
+      displayLibrary();
+    });
+    parentDiv.appendChild(removeButton);
+  }
+  createBookObject() {
+    const bookObject = document.createElement("div");
+    const textWrapper = document.createElement("div");
+    const buttonWrapper = document.createElement("div");
+    bookObject.id = "book";
+    textWrapper.id = "book-text";
+    buttonWrapper.id = "book-buttons";
+    this.appendTitle(textWrapper);
+    this.appendAuthor(textWrapper);
+    this.appendPages(textWrapper);
+    this.appendReadButton(buttonWrapper);
+    this.appendRemoveButton(buttonWrapper);
+    bookObject.appendChild(textWrapper);
+    bookObject.appendChild(buttonWrapper);
+    return bookObject;
+  }
+  getID() {
+    return this.id;
   }
   toString() {
     return (
-      "\n" +
       "Title: " +
       this.title +
-      "\n\n" +
+      "\n" +
       "Author: " +
       this.author +
-      "\n\n" +
+      "\n" +
       "Number of Pages: " +
       this.pages +
-      "\n\n" +
-      "Status: " +
-      this.readToString +
-      "\n"
+      "\n" +
+      "Read: " +
+      this.read
     );
   }
 }
 
 function addBookToLibrary() {
-  // Check if Book present
-  if (Title.value === "" && Author.value === "" && Pages.value === "")
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const read = document.getElementById("read");
+
+  // Check if Book form is filled
+  if (title.value === "" || author.value === "" || pages.value === "")
     return null;
-  // Add Book
-  Library.push(new Book(Title.value, Author.value, Pages.value, Read.value));
+
+  // Add book object to Library
+  const book = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.value,
+    Library.length - 1 // To match current index in array
+  );
+  Library.push(book.createBookObject());
+
   // Unfill Form
-  Title.value = "";
-  Author.value = "";
-  Pages.value = "";
-  Read.checked = false;
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
+
   // Display Library
   displayLibrary();
   toggleOverlay();
 }
 
-function toggleOverlay() {
-  var currentDisplay = Popup.style.display.toString();
-  if (currentDisplay === "none" || currentDisplay.length === 0) {
-    Popup.style.display = "flex";
-  } else Popup.style.display = "none";
+function displayLibrary() {
+  // Create list element if list element isn't already present
+  var list = document.getElementById("list")
+    ? document.getElementById("list")
+    : document.createElement("div");
+
+  list.id = "list";
+  list.replaceChildren(...Library);
+
+  if (!document.getElementById("list"))
+    document.getElementById("library").prepend(list);
+  else if (Library.length === 0) {
+    document.getElementById("list").remove();
+  }
 }
 
-function displayLibrary() {
-  let libraryToString = "";
+function searchLibrary(book) {
   for (let index = 0; index < Library.length; index++) {
-    libraryToString += Library[index].toString();
-    for (let index = 0; index < 25; index++) {
-      libraryToString += "-";
+    if (book.isEqualNode(Library[index])) {
+      return index;
     }
   }
-  List.innerText = libraryToString;
+  return -1;
+}
+
+function toggleOverlay() {
+  const popup = document.getElementById("popup");
+  var currentDisplay = popup.style.display.toString();
+  var bookAdder = document.getElementById("book-adder-img");
+  if (currentDisplay === "none" || currentDisplay.length === 0) {
+    popup.style["display"] = "flex";
+    bookAdder.style["transform"] = "rotate(45deg)";
+  } else {
+    popup.style["display"] = "none";
+    bookAdder.style["transform"] = "rotate(0deg)";
+  }
+}
+
+function changeBackground() {
+  const file = document.querySelector("input[type=file]").files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      var result = "url('" + reader.result + "')";
+      document.body.style["background"] = result;
+      document.body.style["background-size"] = "cover";
+    },
+    false
+  );
+
+  if (file) {
+    console.log(file);
+    reader.readAsDataURL(file);
+  }
+}
+function resetBackground() {
+  document.body.style["background"] = "url(./img/library.png)";
+  document.body.style["background-size"] = "cover";
 }
